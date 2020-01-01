94fb0eb29f95ad50bc444ce61e8696da55b62d83
Robert Kroeger <rjkroege@chromium.org>
2020-01-01 08:07:34 -0500

    Add mime types
    Safari needs files to have valid mime types. Provide mime types based
    on the file extension.

--- cmd.go
+++ cmd.go
@@ -31,6 +31,33 @@ func usage() {
 	flag.PrintDefaults()
 }
 
+// I copied this from blog. It behooves me to create a library that
+// I can use.
+func setMimeType(pth string, w http.ResponseWriter) {
+	ext := path.Ext(pth)
+
+	// Expand this as necessary based on the content that we feature.
+	switch ext {
+	case "":
+		pth = path.Join(pth, "index.html")
+		w.Header().Set("Content-Type", "text/html")
+	case ".html":
+		w.Header().Set("Content-Type", "text/html")
+	case ".xml":
+		w.Header().Set("Content-Type", "application/xml")
+	case ".css":
+		w.Header().Set("Content-Type", "text/css")
+	case ".js":
+		w.Header().Set("Content-Type", "application/javascript")
+	case ".svg":
+		w.Header().Set("Content-Type", "image/svg+xml")
+	case ".jpeg", ".jpg":
+		w.Header().Set("Content-Type", "image/jpeg")
+	case ".png":
+		w.Header().Set("Content-Type", "image/png")
+	}
+}
+
 func main() {
 	log.Println("Hello")
 
@@ -66,7 +93,7 @@ func main() {
 				w.WriteHeader(http.StatusNotFound)
 				return
 			}
-
+			setMimeType(fpath, w)
 			if _, err := io.Copy(w, fd); err != nil {
 				log.Println("failed to serve file", fpath, "because", err)
 				w.WriteHeader(http.StatusBadRequest)
@@ -85,7 +112,7 @@ func main() {
 				w.WriteHeader(http.StatusNotFound)
 				return
 			}
-
+			setMimeType(fpath, w)
 			if _, err := io.Copy(w, fd); err != nil {
 				log.Println("failed to serve file", fpath, "because", err)
 				w.WriteHeader(http.StatusBadRequest)
