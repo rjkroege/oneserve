e18a9c6313e31800211ae0ac43400c0649a96f79
Robert Kroeger <rjkroege@chromium.org>
2020-01-01 07:43:34 -0500

    Augment oneserve to have richer serving semantics
    oneserve can now serve a whole directory hierarchy.

--- cmd.go
+++ cmd.go
@@ -1,47 +1,93 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
-	"flag"
 	"net/http"
 	"os"
+	"path"
+	"path/filepath"
 	"strconv"
 )
 
-
-var portFlag  int
+var portFlag int
 
 func init() {
 	flag.IntVar(&portFlag, "port", 8000, "port number")
+	flag.Usage = usage
+}
+
+const usagestring = `oneserve [<file>...]
+Provide a list of one or more files to seve those and only those
+where the root of the http tree is the cwd where oneserve runs.
+In the absence of arguments, serves all of the files in the cwd.
+
+Paths must be w.r.t. the cwd.
+`
+
+func usage() {
+	os.Stdout.WriteString(usagestring)
+	flag.PrintDefaults()
 }
 
 func main() {
 	log.Println("Hello")
 
-	flag.Parse();
-
+	flag.Parse()
 	args := flag.Args()
+	rootpath, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("can't determine current directory: ", err)
+	}
 
 	for _, fn := range args {
 		log.Println("file to serve", fn)
 
-		http.HandleFunc("/" + fn, func(w http.ResponseWriter, r *http.Request) {
+		bfn, err := filepath.Abs(fn)
+		if err != nil {
+			log.Fatalf("Can't absolute %s: %v\n", fn, err)
+		}
+		relfn, err := filepath.Rel(rootpath, bfn)
+		if err != nil {
+			log.Fatalf("Bad path %s: %v\n", fn, err)
+		}
+		if len(relfn) > 1 && relfn[0:2] == ".." {
+			log.Fatalf("%s not allowed to climb out of %s\n", relfn, rootpath)
+		}
+
+		http.HandleFunc(path.Join("/", relfn), func(w http.ResponseWriter, r *http.Request) {
 			log.Println(r.URL.Path)
-			relpth := r.URL.Path[1:]
-			
-			fd, err  := os.Open(relpth)
+			fpath := filepath.Join(rootpath, r.URL.Path)
+
+			fd, err := os.Open(fpath)
 			if err != nil {
-				// should fail out in some way?
-				log.Println("Coudln't open", relpth, "because", err)
-				// write something...
-				
+				log.Println("Coudln't open", fpath, "because", err)
 				w.WriteHeader(http.StatusNotFound)
 				return
 			}
 
 			if _, err := io.Copy(w, fd); err != nil {
-				log.Println("failed to serve file",  relpth, "because", err)
+				log.Println("failed to serve file", fpath, "because", err)
+				w.WriteHeader(http.StatusBadRequest)
+			}
+		})
+	}
+
+	if len(args) == 0 {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			log.Println(r.URL.Path)
+			fpath := filepath.Join(rootpath, r.URL.Path)
+
+			fd, err := os.Open(fpath)
+			if err != nil {
+				log.Println("Coudln't open", fpath, "because", err)
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+
+			if _, err := io.Copy(w, fd); err != nil {
+				log.Println("failed to serve file", fpath, "because", err)
 				w.WriteHeader(http.StatusBadRequest)
 			}
 		})
@@ -49,6 +95,5 @@ func main() {
 
 	network := ":" + strconv.Itoa(portFlag)
 	log.Fatal(http.ListenAndServe(network, nil))
-
 
 }
